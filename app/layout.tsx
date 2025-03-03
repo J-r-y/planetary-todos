e9068a498fbf91e874f5ff6b4c10fa6cd9e34616
J-r-y <jry@nix.com>
2025-03-03 17:16:33 +0100

    dark mode toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Poppins, Roboto } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "./components/theme-provider";
+import { ModeToggle } from "./components/ModeToggle";
 
 const poppins = Poppins({
 	weight: '400',
@@ -23,11 +25,19 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<body
 				className={`${roboto.className} ${poppins.className} antialiased`}
 			>
-				{children}
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<ModeToggle />
+					{children}
+				</ThemeProvider>
 			</body>
 		</html>
 	);
